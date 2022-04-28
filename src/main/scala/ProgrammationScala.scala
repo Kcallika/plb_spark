be3283870a45651a0195b099a49520a8f5e50cfb
Kcallika <ksenia17lirian01@gmail.com>
2022-04-28 11:59:43 +0200

    conditions

--- src/main/scala/ProgrammationScala.scala
+++ src/main/scala/ProgrammationScala.scala
@@ -1,3 +1,6 @@
+// import scala.collection.mutable
+import scala.collection.immutable
+
 object ProgrammationScala {
 
    // public variable of base type (not class type)
@@ -24,6 +27,9 @@ object ProgrammationScala {
     testHello("Youpi!")
     val res = testABC(10, 150)
     println(res)
+
+
+    structureData()
   }
 
   def fonction2() : Unit = {
@@ -74,5 +80,68 @@ object ProgrammationScala {
     } else {
       println("i is smaller")
     }
+  }
+
+  // collections : list, tuple, set, map
+  // list: immutable, organised (ordered --- you can access with idx) and data is of same type
+
+  def structureData() : Unit = {
+    // lists
+
+    // lists with Int, Strig and Range
+    val list1 : List[Int] = List(1,8, 5, 6, 9, 59)
+    val list_names : List[String] = List("Oxana", "Alex", "Valentin")
+    val nums = List.range(0, 15)
+
+    // manipulation
+    for (name  <- list_names) {
+      println(name)
+    }
+
+    println("====================")
+
+    // anonymous function as python lambda
+    val list_maj : List[String] = list_names.map(e => e.toUpperCase)
+    list_maj.foreach(e => println(e) )
+
+    println("====================")
+    // all names by V
+    val list_v = list_names.map(i => i.startsWith("V")) // applies on each elem we could use filter instead
+    list_v.foreach(i => println(i))
+
+    // list * 2
+    val list_mult = list1.map(i => i*2 )
+    list_mult.foreach(i => println(i))
+
+    // list val greater 5
+    val list_greater :  List[Int] = list1.filter(_ > 5) // i => i > 5
+    list_greater.foreach(i => println(i))
+  }
+
+
+  // tuples
+  // values are from different types
+  val my_tuple: (String, Int, Boolean) = ("oxa", 49, true)
+  val other_tuple = (45, "lion", "unicorn", false)
+  val all_tuple = ("kitten", 24, Vehicule(MATRICULE_ID = "hkk", COLOUR = "red", TRANSMISSION_TYPE = "auto", ENGINE_TYPE = "turbo"))
+
+  println(other_tuple._3.toUpperCase)
+
+  // map of Hash Tab
+  val cities : Map[String, String] = Map(
+    "PS" -> "Paris",
+    "LS" -> "Lyon",
+    "MA" -> "Marseille"
+  )
+
+  cities.foreach(k  => println("key" + k._1 + " value" + k._2))
+
+  // tables : mutable lists
+  val tab : Array[String] = Array("Oxana", "Alex", "Valentin", "Unicorn")
+  tab(0) = "Julien"
+
+  var i = 0
+  for (i <- 0 to 3 ) {
+    println(tab(i))
   }
 }
